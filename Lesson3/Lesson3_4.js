e3a072c110fd8431e7a7064eaa15448d29b8e989
Illarion-pb <illarion.chesnokov@privatbank.ua>
2022-01-19 12:34:53 +0200

    Lesson3_4

---
+++ Lesson3/Lesson3_4.js
@@ -0,0 +1,40 @@
+var f = function(val) {
+    if (isNaN(val)) {
+        throw new Error('parameter type is not a Number');
+    }
+    if ((val < 1) || (val > 7)) {
+        throw new Error('parameter should be in the range of 1 to 7');
+    }
+    switch (val) {
+        case 1:
+            console.log('Понедельник');
+            break;
+        case 2:
+            console.log('Вторник');
+            break;
+        case 3:
+            console.log('Среда');
+            break;
+        case 4:
+            console.log('Четверг');
+            break;
+        case 5:
+            console.log('Пятница');
+            break;
+        case 6:
+            console.log('Суббота');
+            break;
+        case 7:
+            console.log('Воскресенье');
+            break;    
+    }
+}
+
+try {
+    f(1);
+    f(5);
+    f(8);
+} catch (e) {
+    console.log('Error: ' + e.message);
+}
+
