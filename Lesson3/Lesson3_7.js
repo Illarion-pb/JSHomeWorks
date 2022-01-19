9d29d46dba2b376e324dcd7f0b92944a6df8abc6
Illarion-pb <illarion.chesnokov@privatbank.ua>
2022-01-19 13:06:59 +0200

    Lesson3_7

---
+++ Lesson3/Lesson3_7.js
@@ -0,0 +1,23 @@
+var getDivisors = function(val) {
+    if (isNaN(val)) {
+        throw new Error('parameter type is not a Number');
+    }
+    if (val <= 0) {
+        throw new Error('parameter can\'t be a 0');
+    }
+    var arr = [];
+    for (var i = 1; i <= val; i++) {
+        if (val % i == 0) {
+            arr.push(i);
+        }
+    }
+    return arr;
+}
+
+try {
+    console.log(getDivisors(12));
+    console.log(getDivisors('Content'));
+    console.log(getDivisors(0));
+} catch (e) {
+    console.log('Error: ' + e.message);
+}
