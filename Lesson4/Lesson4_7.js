d636d5ae667de6e4baa148fdd3bc7b33f4eb335e
Illarion-pb <illarion.chesnokov@privatbank.ua>
2022-01-26 14:59:11 +0200

    Lesson4_7

--- Lesson4/Lesson4_7.js
+++ Lesson4/Lesson4_7.js
@@ -3,7 +3,7 @@ let arrLength = [];
 for (let i = 0; i < arr.length; i++) {
     arrLength[i] = arr[i].length;
 }
-console.log( arrLength ); // 4,5,2,3
+console.log( arrLength ); // [ 4, 5, 2, 3 ]
 arrLength = [];
 console.log(arrLength); //[]
 arrLength = ['abcd', 'abcde', 'ab', 'abc', 'new_value'].map(item => item.length);
